c3cddf1b71350941c5241b0138894e752ca1b419
Floweynt <50057682+Floweynt@users.noreply.github.com>
2026-06-28 23:53:54 -0500

    read from env

--- bot/config.ts
+++ bot/config.ts
@@ -13,8 +13,10 @@ let config: BotConfig | undefined;
 export function getConfig() {
     if (config === undefined) {
         config = JSON.parse(readFileSync("config.json").toString("utf8")) as BotConfig;
-        assert(typeof config.token === "string");
-        assert(typeof config.id === "string");
+        config.token = process.env.BOT_TOKEN ?? config.token;
+        config.id = process.env.BOT_ID ?? config.id;
+        assert(typeof config.token === "string", "token must be set in config.json or BOT_TOKEN env var");
+        assert(typeof config.id === "string", "id must be set in config.json or BOT_ID env var");
     }
 
     return config;
